0fb55279e6cd1946a7a6c34e2efce1694ebec507
yangfukai1714 <315774627@qq.com>
2020-08-19 19:13:27 +0800

    autobox的演示

--- IdeaStudyCode/src/main/java/study/lang/basicType/AutoBox.java
+++ IdeaStudyCode/src/main/java/study/lang/basicType/AutoBox.java
@@ -7,5 +7,20 @@ package study.lang.basicType;
  * @date 2020/8/19
  */
 public class AutoBox {
-    
+    public static void main(String[] args) {
+
+        autoBoxInt();
+    }
+
+    static  void autoBoxInt(){
+        int i = 40;
+        Integer i1 = i;//java/lang/Integer.valueOf>
+        int x = i1;//<java/lang/Integer.intValue>
+        Integer i4 = new Integer(40);
+    }
+//    int i = 40;
+//    Integer i1 = Integer.valueOf(i);
+//    int x = i1;
+//        new Integer(40);
+
 }
